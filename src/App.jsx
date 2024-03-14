eb1aff444a37bde1694243057440350ab7f67465
thatcatfromspace <encyclopedia748@gmail.com>
2024-03-14 12:00:14 +0530

    boileplate +setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="main bg-gradient-to-b from-[#C0FFE1] to-[#FCFCFC] [height:100%] [min-height:100vh] font-inter">
+      login thingy
+    </div>
+  )
+}
+
+export default App;
